698db129132db783c3c3da9e231407811bf112c5
wwqwlas <wwqwlas@gmail.com>
2025-07-15 21:43:13 -0300

    Update myext2.js

--- myext2.js
+++ myext2.js
@@ -12,23 +12,16 @@
           {
             opcode: 'myBlock3',
             blockType: Scratch.BlockType.EVENT,
-            text: 'when page loads',
+            text: 'when mouse clicks',
           },
         ]
       };
     }
     myBlock3(args) {
       {
-      // This block should trigger when the page is fully loaded.
-      // We can't directly trigger a hat block, so we'll use a workaround:
-      // 1. Store a value in a variable to indicate the page is loaded.
-      // 2. Use a separate "when flag clicked" script that checks the variable.
-      
-      // Set a variable in the Scratch project to indicate the page has loaded.
-      // The variable must be named "pageLoaded" for this to work.
-      // This ensures that a "when flag clicked" script can react when the page is ready.
-      this.runtime.ext_scratch3_variables.createVariable('pageLoaded', 'true');
-      return;
+      document.addEventListener('mousedown', function() {
+      runtime.startHats('utility_myBlock3');
+      });
       }
     }
   }
